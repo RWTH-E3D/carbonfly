b53ff6f9d839ff6866ef3625dac7ff446fd27f2b
hqr <71382064+hqrrr@users.noreply.github.com>
2025-10-23 18:11:14 +0200

    Add version info

---
+++ carbonfly/__init__.py
@@ -0,0 +1,12 @@
+"""
+carbonfly
+    a lightweight, easy-to-use Python API and
+    toolbox for indoor CO2 CFD simulations in Grasshopper
+    based on OpenFOAM and WSL
+
+- Author: Qirui Huang
+- License: LGPL-3.0
+- Website: https://github.com/RWTH-E3D/carbonfly
+"""
+
+__version__ = "0.6.0"
